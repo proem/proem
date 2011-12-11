8373be76598e64dfcf92b2441758549c671b53da
Tony R Quilkey <thorpe@thorpesystems.com>
2011-12-11 19:43:07 +1100

    Add bootstrap for use with tests

---
+++ tests/bootstrap-dist.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'lib/Proem/Loader/Autoloader.php';
+
+use Proem\Loader\Autoloader;
+
+$loader = new AutoLoader();
+$loader->registerNamespaces([
+    'Proem\\Tests'  => 'tests/lib',
+    'Proem'       => 'lib',
+    'PHPUnit_'      => '/usr/share/php/PHPUnit'
+])->register();

b45916d3b76051fd6077ccff539ab8593eb403ea
Tony R Quilkey <thorpe@thorpesystems.com>
2012-01-03 22:14:31 +1100

    Fix tests bootstrap-dist.php file to use new location of the Autoloader

--- tests/bootstrap-dist.php
+++ tests/bootstrap-dist.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once 'lib/Proem/Loader/Autoloader.php';
+require_once 'lib/Proem/Api/Autoloader.php';
 
-use Proem\Loader\Autoloader;
+use Proem\Api\Autoloader;
 
 $loader = new AutoLoader();
 $loader->registerNamespaces([
     'Proem\Tests'   => 'tests/lib',
-    'Proem'         => 'lib'
+    'Proem'         => ['tests/lib/Proem/Tests/Fixtures', 'lib']
 ])->register();
